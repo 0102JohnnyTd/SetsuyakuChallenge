e24b20481afea4a32a97ffa39f402078e2fce27d
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-10 22:35:15 +0900

    3つのtextFieldのdelegateの通知先を順次SignUpViewControllerに設定する処理の実装

--- SetsuyakuChallenge/SignUpViewController.swift
+++ SetsuyakuChallenge/SignUpViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+final class SignUpViewController: UIViewController {
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var userNameTextField: UITextField!
+
+    private var textFields: [UITextField] { [emailTextField, passwordTextField, userNameTextField] }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpTextFileds()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpTextFileds() {
+        textFields.forEach { $0.delegate = self }
     }
-    */
+}
 
+extension SignUpViewController: UITextFieldDelegate {
 }

3eaf7eb376adf52ef5e84a533f800bb72d75f2a8
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-12 15:41:23 +0900

    TableViewに表示させるCellのXIBファイルとクラスファイルを作成

---
+++ SetsuyakuChallenge/UserDetailsTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  UserDetailsTableViewCell.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/12.
+//
+
+import UIKit
+
+final class UserDetailsTableViewCell: UITableViewCell {
+}

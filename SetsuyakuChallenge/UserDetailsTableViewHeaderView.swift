035295b46e97cd9e89e172adb494587a5bed728a
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-12 12:42:45 +0900

    UserDetailsViewControllerに設置予定のTableViewに表示させるUITableViewHeaderFooterViewのXIBファイルとクラスファイルを作成

---
+++ SetsuyakuChallenge/UserDetailsTableViewHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  UserDetailsTableViewHeaderView.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/12.
+//
+
+import UIKit
+
+class UserDetailsTableViewHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

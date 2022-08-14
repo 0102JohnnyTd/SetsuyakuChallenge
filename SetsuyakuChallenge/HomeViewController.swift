385f0b283aec1af011c11b00b309333aa332c6cb
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-14 18:19:36 +0900

    ホーム画面のStoryboardファイルとクラスファイルを作成

---
+++ SetsuyakuChallenge/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/14.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

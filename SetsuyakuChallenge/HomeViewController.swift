83ec3ed85911ca911e13779d9d373886d1ef878f
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-16 17:36:26 +0900

    サインアウト状態だった場合、アプリ起動時にsignUpViewControllerを表示させる処理の実装

--- SetsuyakuChallenge/HomeViewController.swift
+++ SetsuyakuChallenge/HomeViewController.swift
@@ -6,9 +6,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 final class HomeViewController: UIViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkIsLogin()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    private func checkIsLogin() {
+        if Auth.auth().currentUser == nil {
+            showSignUpVC()
+        }
+        print("現在ログイン状態です")
+    }
+
+    private func showSignUpVC() {
+        print(#function)
+
+        let signUpVC = UIStoryboard(name: SignUpViewController.storyboardName, bundle: nil).instantiateViewController(withIdentifier: SignUpViewController.identifier) as! SignUpViewController
+        let nav = UINavigationController(rootViewController: signUpVC)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true)
     }
 }

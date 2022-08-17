3e7841fd46d1b428beb5565263a4d8c0659811fb
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-17 11:36:20 +0900

    HomeViewControllerに表示させるCollectionViewCellのXIBファイルとクラスファイルを作成

---
+++ SetsuyakuChallenge/ChallengeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChallengeCollectionViewCell.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/17.
+//
+
+import UIKit
+
+class ChallengeCollectionViewCell: UICollectionViewCell {
+    
+}

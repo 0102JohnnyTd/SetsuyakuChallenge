5bc402861af444db66066f3e8879fd722e989bc9
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-22 14:11:54 +0900

    ViewからControllerへUserActionを通知するために@IBOutletを接続

--- SetsuyakuChallenge/SignInViewController.swift
+++ SetsuyakuChallenge/SignInViewController.swift
@@ -8,22 +8,11 @@
 import UIKit
 
 final class SignInViewController: UIViewController {
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

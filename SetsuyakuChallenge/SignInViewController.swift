2aedeee5e37f885d7ddfbda284c32e179ff40639
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-22 14:06:55 +0900

    カプセル化対応の為、サインイン用の画面のクラスファイルに修飾子finalを追加

--- SetsuyakuChallenge/SignInViewController.swift
+++ SetsuyakuChallenge/SignInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+final class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

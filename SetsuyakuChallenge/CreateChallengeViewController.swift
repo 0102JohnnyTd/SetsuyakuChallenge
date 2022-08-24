5ae0d4ef7a899daa223a8e64e8af15a2d435c7b6
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-24 16:39:18 +0900

    ボタンに丸みを加え、backgroundColorをアプリのmain colorに設定する処理を定義して実装

--- SetsuyakuChallenge/CreateChallengeViewController.swift
+++ SetsuyakuChallenge/CreateChallengeViewController.swift
@@ -18,19 +18,10 @@ final class CreateChallengeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpButtonContents()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpButtonContents() {
+        createChallengeButton.backgroundColor = .mainColor()
+        createChallengeButton.layer.cornerRadius = 5
     }
-    */
-
 }

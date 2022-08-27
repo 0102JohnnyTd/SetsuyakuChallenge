153a1229bd1b6e2573a54d0d89aace7a0e2ce43a
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-27 16:31:28 +0900

    ChallengeCollectionViewCellのconfigureメソッドの引数に渡す値をChallengeモデルのarrayの要素に変更

--- SetsuyakuChallenge/HomeViewController.swift
+++ SetsuyakuChallenge/HomeViewController.swift
@@ -65,7 +65,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = challengeCollectionView.dequeueReusableCell(withReuseIdentifier: ChallengeCollectionViewCell.identifier, for: indexPath) as! ChallengeCollectionViewCell
-        cell.configure(itemName: "お菓子", goalPrice: String(100), itemImage: UIImage(named: "Default Item Image")!)
+        cell.configure(itemName: Challenge.array[indexPath.row].itemName, goalPrice: Challenge.array[indexPath.row].itemPrice, itemImage: Challenge.array[indexPath.row].itemImage)
 
         return cell
     }

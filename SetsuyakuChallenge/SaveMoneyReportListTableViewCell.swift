38b99c5854402fc6acfcc2d0485ccbb8dfb37089
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-29 21:35:38 +0900

    storyboard上のTableViewに表示させるCellのXIBファイルとそのクラスファイルをプロジェクトに追加

---
+++ SetsuyakuChallenge/SaveMoneyReportListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SaveMoneyReportListTableViewCell.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/29.
+//
+
+import UIKit
+
+class SaveMoneyReportListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

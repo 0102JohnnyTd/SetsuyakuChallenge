4d2f5765ddcea413422e3dc0a10f25818c20cf82
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-30 18:10:05 +0900

    ViewからControllerへUserActionを通知するために@IBOutletを利用してstoryboard上のオブジェクトとクラスファイルを紐づけ

--- SetsuyakuChallenge/CreateSaveMoneyReportViewController.swift
+++ SetsuyakuChallenge/CreateSaveMoneyReportViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class CreateSaveMoneyReportViewController: UIViewController {
+    @IBOutlet private weak var priceTextField: UITextField!
+
+    @IBOutlet private weak var memoTextView: UITextView!
+
+    @IBOutlet private weak var createReportButton: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()

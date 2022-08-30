c15d469dafe90021ff974c0e3baec55655c81515
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-30 17:38:58 +0900

    節約レポートを記録する画面のstoryboardとクラスファイルをプロジェクトに追加

---
+++ SetsuyakuChallenge/CreateSaveMoneyReportViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateSaveMoneyReportViewController.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/08/30.
+//
+
+import UIKit
+
+class CreateSaveMoneyReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

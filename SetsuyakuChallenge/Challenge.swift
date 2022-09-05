e4ee42856182f2cb0cc953e576c8d1b821a0d519
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-05 15:18:24 +0900

    辞書型のKeyと一致するValueをプロパティの初期値として代入するイニシャライザを作成

--- SetsuyakuChallenge/Challenge.swift
+++ SetsuyakuChallenge/Challenge.swift
@@ -14,4 +14,10 @@ struct Challenge {
     var goalAmount: Int
 
     static var array: [Challenge] = []
+
+    init(dic: [String: Any]) {
+        self.imageURL = dic[ChallengesDocDataKey.imageURL] as! String
+        self.name = dic[ChallengesDocDataKey.name] as! String
+        self.goalAmount = dic[ChallengesDocDataKey.goalAmount] as! Int
+    }
 }

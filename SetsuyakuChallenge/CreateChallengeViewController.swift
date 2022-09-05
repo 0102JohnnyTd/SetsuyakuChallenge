065b49943dafa6803d15347f763d70b1f7118b23
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-05 15:37:44 +0900

    viewWillAppearでfetchChallengeDataメソッドを呼ぶ処理の実装

--- SetsuyakuChallenge/CreateChallengeViewController.swift
+++ SetsuyakuChallenge/CreateChallengeViewController.swift
@@ -26,6 +26,10 @@ final class CreateChallengeViewController: UIViewController {
 
     private var textFields: [UITextField] { [nameTextField, goalAmountTextField] }
 
+    override func viewWillAppear(_ animated: Bool) {
+        fetchChallengeData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpButton()

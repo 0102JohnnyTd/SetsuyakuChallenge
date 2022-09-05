82a50212fb4cf72828f9f83911f98f6e23a5b0ff
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-05 13:52:07 +0900

    ハードコーディング対応として、Firestorageのファイル名やFirestoreのcollection名、key名を管理するenumを生成

---
+++ SetsuyakuChallenge/FirebaseValueManager.swift
@@ -0,0 +1,29 @@
+//
+//  FirebaseValueManager.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/09/05.
+//
+
+import Foundation
+
+enum StorageFileName {
+    static let itemImage = "item_image"
+}
+
+enum CollectionName {
+    static let users = "users"
+    static let challenges = "challenges"
+}
+
+enum UsersDocDataKey {
+    static let email = "email"
+    static let name = "name"
+    static let createdAt = "createdAt"
+}
+
+enum ChallengesDocDataKey {
+    static let imageURL = "imageURL"
+    static let name = "name"
+    static let price = "price"
+}

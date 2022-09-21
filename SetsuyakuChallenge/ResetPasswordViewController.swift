f624d6276f40efc257ff6bf41464ea75e24d21db
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-21 12:56:02 +0900

    SendEmailButtonをタップした時パスワード再設定の案内メールを送信する処理を実装

--- SetsuyakuChallenge/ResetPasswordViewController.swift
+++ SetsuyakuChallenge/ResetPasswordViewController.swift
@@ -6,17 +6,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 final class ResetPasswordViewController: UIViewController {
     @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var sendEmailButton: UIButton!
     @IBAction private func didTapButton(_ sender: Any) {
+        sendResetPasswordEmail()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTextField()
         setUpButton()
+    }
+
+    private func sendResetPasswordEmail() {
+        guard let email = emailTextField.text else { return }
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("送信エラーです： \(error.localizedDescription)")
+            } else {
+                print("\(email)宛にメールを送信しました。")
+            }
+        }
     }
 
     private func setUpTextField() {

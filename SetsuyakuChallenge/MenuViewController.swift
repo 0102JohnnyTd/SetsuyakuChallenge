4080fed158ffdc2e94a8743a4f8d81b51d326e7d
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-22 14:54:11 +0900

    利用規約やプライバシーポリシー、お問い合わせフォームなどのメニューを表示する画面のstoryboardとクラスファイルを作成

---
+++ SetsuyakuChallenge/MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/09/22.
+//
+
+import UIKit
+
+final class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

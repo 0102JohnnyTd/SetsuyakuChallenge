4cd05e1122609d03785c1c0721a93d45b5d4fd9e
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-22 16:34:41 +0900

    MenuListのTableView上に表示させるCellのXIBファイルとクラスファイルをプロジェクトに追加

---
+++ SetsuyakuChallenge/MenuListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuListTableViewCell.swift
+//  SetsuyakuChallenge
+//
+//  Created by Johnny Toda on 2022/09/22.
+//
+
+import UIKit
+
+final class MenuListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

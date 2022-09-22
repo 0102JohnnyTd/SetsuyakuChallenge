bbe4dd272eeaf88fe21f98f13983df562dae4ff9
0102johnny <rt2824.recruit.1022@gmail.com>
2022-09-22 16:22:55 +0900

    命名を修正

--- SetsuyakuChallenge/MenuListViewController.swift
+++ SetsuyakuChallenge/MenuListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MenuViewController.swift
+//  MenuListViewController.swift
 //  SetsuyakuChallenge
 //
 //  Created by Johnny Toda on 2022/09/22.
@@ -7,8 +7,8 @@
 
 import UIKit
 
-final class MenuViewController: UIViewController {
-    @IBOutlet private weak var menuListTableViewCell: UITableView!
+final class MenuListViewController: UIViewController {
+    @IBOutlet private weak var menuListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
